270f7a61bbddf8e7ccea062ca92e387fd722924e
augustoristow1 <augustoristow@icloud.com>
2025-06-21 14:54:39 -0300

    Fixed SSLScanner.java
    Corrigido algumas linhas que estavam travando a interface

--- SSLScanner.java
+++ SSLScanner.java
@@ -12,24 +12,24 @@ public class SSLScanner {
         logArea.appendText("[+] Iniciando SSLScanner para " + url + "\n");
 
         try {
-            // Tenta se conectar usando HTTPS
+            
             URL httpsUrl = new URL("https://" + url);
             HttpsURLConnection conn = (HttpsURLConnection) httpsUrl.openConnection();
             conn.setConnectTimeout(5000);
             conn.connect();
 
-            // Se chegou até aqui, SSL está disponível
+            
             logArea.appendText("➤ Certificado SSL encontrado ✅\n");
             logArea.appendText("➤ Protocolo: " + conn.getCipherSuite() + "\n");
 
-            ResultAggregator.addResult("SSL", 0); // Sem problema
+            ResultAggregator.addResult("SSL", 0); 
 
         } catch (IOException e) {
-            // Caso erro na conexão HTTPS (SSL inexistente)
+            
             logArea.appendText("❌ SSL NÃO DISPONÍVEL no site " + url + "\n");
             logArea.appendText("➤ Detalhes do erro: " + e.getMessage() + "\n");
 
-            ResultAggregator.addResult("SSL", 1); // Um problema encontrado
+            ResultAggregator.addResult("SSL", 1); 
         }
 
         logArea.appendText("[+] SSLScanner finalizado para " + url + "\n");
